25521e290879f1da4d39cfccd0535685528add46
Ian Dunn <itd3516@rit.edu>
2025-08-24 16:26:01 -0400

    I hate single quotes

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>Ian Dunn</h1>
-      <p class="mb-3">I'm a software engineer specializing in enterprise backend systems and web development.</p>
+      <p className="mb-3">{"I'm a software engineer specializing in enterprise backend systems and web development."}</p>
       <section>
         <ul>
           <li><a href="/resume">Resume</a></li>
