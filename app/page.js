e5081c25089acd2d34324d91f712a0db514d1edd
Ian Dunn <itd3516@rit.edu>
2025-08-24 18:52:39 -0400

    Add padding to sides

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className="px-5">
       <h1>Ian Dunn</h1>
       <p className="mb-2">{"I'm a software engineer specializing in enterprise backend systems and web development."}</p>
       <section>
